1678781424627baf27c23ea11bbd86f45dc4b9ed
W2020T <wtkui20001102@icloud.com>
2022-03-03 17:13:01 +0900

    URLの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root to: 'home#top'
-  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/:provider/callback' => 'sessions#create'
 
   get 'auth/failure' => 'users#login_form'
   resources :home, only: %i[top]
