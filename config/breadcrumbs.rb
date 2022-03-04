87f58ce1b80cc3f8f8e5db5d77e6509d386b5b1e
W2020T <wtkui20001102@icloud.com>
2022-03-04 12:48:26 +0900

    記述の追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,28 +1,27 @@
 crumb :root do
-  link "Home", root_path
+  link 'Home', posts_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :users_show do
+  link 'ユーザー詳細', user_path
+  parent :root
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :user_edit do
+  link 'ユーザー編集'
+  parent :user_show
+end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
+crumb :post_new do
+  link '新しいクチコミ投稿', new_post_path
+  parent :root
+end
 
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+rumb :post_show do
+  link '投稿詳細', post_path
+  parent :root
+end
+crumb :user do |user|
+  link user.name, user_path(user)
+  parent :root
+end
