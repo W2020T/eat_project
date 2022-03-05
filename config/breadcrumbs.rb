de30a4b6df3ac70feb1a382493ea584b65cf1792
W2020T <wtkui20001102@icloud.com>
2022-03-05 22:49:08 +0900

    記述の追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,39 +1,61 @@
-crumb :root do
-  link 'Home', posts_path
+crumb :posts do
+  link '投稿一覧', 'posts/index'
 end
-
+crumb :users do
+  link 'ユーザ一覧', users_path
+end
 crumb :users_show do
   link 'ユーザー詳細', users_path
-  parent :root
+  parent :users
 end
 
 crumb :user_edit do
   link 'ユーザー編集'
-  parent :user_show
+  parent :users_show
 end
 
 crumb :posts_new do
-  link '新しいクチコミ投稿', new_post_path
-  parent :root
+  link '新しい投稿', new_post_path
+  parent :posts
 end
 
 rumb :posts_show do
   link '投稿詳細', posts_path
-  parent :root
+  parent :posts
 end
 crumb :post_edit do
   link '投稿編集'
   parent :post_show
 end
 crumb :user do |user|
-  link user.name, user_path(user)
+  link "#{user.name}さんの詳細", user_path(user)
   parent :root
 end
-crumb :name_search do |search|
+crumb :user do |user|
+  link "#{user.name}さんのフォロワー", user_path(user)
+  parent :users_show
+end
+crumb :user do |user|
+  link "#{user.name}さんの詳細", user_path(user)
+  parent :users_show
+end
+crumb :content_search do |search|
   if search == ''
-    link 'クチコミ検索結果', posts_search_path
+    link '検索結果', posts_search_path
   else
-    link "「#{search}」のクチコミ検索結果", posts_search_path
+    link "「#{search}」の検索結果", posts_search_path
   end
+  parent :posts
+end
+crumb :users_search do |search|
+  if search == ''
+    link '検索結果', posts_search_path
+  else
+    link "「#{search}」の検索結果", posts_search_path
+  end
+  parent :posts
+end
+crumb :post do |post|
+  link post.content, post
   parent :posts
 end
