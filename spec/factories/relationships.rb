68dc01dd45493bf35f3a9519288bee9ff51ed9de
W2020T <wtkui20001102@icloud.com>
2022-03-15 12:29:19 +0900

    ファイルの編集

--- spec/factories/relationships.rb
+++ spec/factories/relationships.rb
@@ -1,8 +1,1 @@
 # frozen_string_literal: true
-
-#FactoryBot.define do
- # factory :relationship do
-  #  follower_id    { FactoryBot.build(:user).id }
-  #  followed_id    { FactoryBot.build(:user).id }
-  #end
-#end
