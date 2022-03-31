3b54cf384d2759b02fe70695defb34605bce949b
W2020T <wtkui20001102@icloud.com>
2022-03-31 15:14:26 +0900

    ファイルの編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :rbenv_ruby, '2.6.5'
 set :branch, 'main'
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, '/etc/nginx/conf.d'
+set :linked_files, %w[config/master.key]
 append :linked_files, 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules'
 
