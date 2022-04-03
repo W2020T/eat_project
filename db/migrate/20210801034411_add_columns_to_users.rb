845061e3dae89a4c736febaf890deea852e9ce6e
W2020T <wtkui20001102@icloud.com>
2022-04-03 17:37:25 +0900

    ライセンスの追加

--- db/migrate/20210801034411_add_columns_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddColumnsToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :password, :string
-    add_column :users, :image_name, :string
-  end
-end
