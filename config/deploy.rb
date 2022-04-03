ca003aaafad741a326dfd103c18dd964743efa44
W2020T <wtkui20001102@icloud.com>
2022-04-03 14:00:11 +0900

    不要な記述の削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_module
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/var/www/eat_project'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
