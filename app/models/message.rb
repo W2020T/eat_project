7eff678b63359b24850c1fc2af7dd4b50b1d28bf
W2020T <wtkui20001102@icloud.com>
2022-05-17 20:27:06 +0900

    バリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+  default_scope -> { order(created_at: :asc) }
+  validates :from_id, presence: true
+  validates :to_id, presence: true
+  validates :content, presence: true, length: { maximum: 300 }
 end
