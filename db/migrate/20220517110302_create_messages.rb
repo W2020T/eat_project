31a76f913c2885ff5cc14df8cb81ee5e0be2fce4
W2020T <wtkui20001102@icloud.com>
2022-05-18 18:25:39 +0900

    ファイルの編集

--- db/migrate/20220517110302_create_messages.rb
+++ db/migrate/20220517110302_create_messages.rb
@@ -11,26 +11,4 @@ class CreateMessages < ActiveRecord::Migration[6.1]
     add_index :messages, :from_id
     add_index :messages, :to_id
   end
-
-  def create
-    @message = current_user.outgoing_messages.build(message_params)
-    @message.to_id = params[:id]
-    @message.save
-    @messages = Message.where('from_id IN (:ids) AND to_id IN (:ids)', ids: @ids)
-    respond_to do |format|
-      format.html { redirect_to messages_urL(@to_user) }
-      format.js { render 'create' }
-    end
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:content)
-  end
-
-  def setup_users
-    @to_user = User.find(params[:id])
-    @ids = [@to_user.id, current_user.id]
-  end
 end
