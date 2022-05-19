7c8d6afbfa799b619294949b01e31a49f57a577e
W2020T <wtkui20001102@icloud.com>
2022-05-19 22:04:06 +0900

    ファイルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_secure_password
   validates :name, { presence: true }
   validates :email, { presence: true, uniqueness: true }
 
-  has_secure_password
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
