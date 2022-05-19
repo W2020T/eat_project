66efbec43f88db5fd81227b5f4adde6c0d89ca72
W2020T <wtkui20001102@icloud.com>
2022-05-19 09:53:59 +0900

    ファイルの編集

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,4 @@
 class Message < ApplicationRecord
-  default_scope -> { order(created_at: :asc) }
-  validates :from_id, presence: true
-  validates :to_id, presence: true
-  validates :content, presence: true, length: { maximum: 300 }
+  belongs_to :user
+  belongs_to :room
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_110302) do
+ActiveRecord::Schema.define(version: 2022_05_18_093521) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,12 @@ ActiveRecord::Schema.define(version: 2022_05_17_110302) do
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "rooms", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tag_maps", charset: "utf8mb4", force: :cascade do |t|
