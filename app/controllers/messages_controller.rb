d2996dcbcb9158779caff7080e5d14d60d04fe15
W2020T <wtkui20001102@icloud.com>
2022-05-19 09:55:25 +0900

    controllerファイルの編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,13 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create]
 
   def create
-    message = Message.new(message_params)
-    message.user_id = current_user.id
-    if message.save
-      redirect_to room_path(message.room)
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      @message = Message.create(params.require(:message).permit(:user_id, :content,
+                                                                :room_id).merge(user_id: current_user.id))
     else
-      redirect_back(fallback_location: root_path)
+      flash[:alert] = 'メッセージ送信に失敗しました。'
     end
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:room_id, :body)
+    　　redirect_to "/rooms/#{@message.room_id}"
   end
 end
