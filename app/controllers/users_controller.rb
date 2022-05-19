46ebeca1eea49a0e076f57659018912e32e5afe9
W2020T <wtkui20001102@icloud.com>
2022-05-19 12:34:24 +0900

    controllerファイルの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,13 +29,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @currentUserEntry=Entry.where(user_id: current_user.id)
-    @userEntry=Entry.where(user_id: @user.id)
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
     if @user.id == current_user.id
     else
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
-          if cu.room_id == u.room_id then
+          if cu.room_id == u.room_id
             @isRoom = true
             @roomId = cu.room_id
           end
@@ -47,7 +47,6 @@ class UsersController < ApplicationController
         @entry = Entry.new
       end
     end
-  end
   end
 
   def update
